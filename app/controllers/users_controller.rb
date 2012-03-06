b90d0b38bc1527590cc33cee06ee8f1abf2d2a79
Robsteranium <robin@robsteranium.plus.com>
2012-03-06 01:56:55 +0000

    return user validation errors to combined user/sessions new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,16 @@ class UsersController < ApplicationController
         format.html { redirect_to name_user_path(current_user.username), :notice => 'Registration successfull.' }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
-        format.html { render :action => "new" } # TODO_HELP: How do I redirect this to 'user_sessions/new' without losing validation errors? user_session/new and user/new now occupy the same view (combined login and signup). Am I even doing that the right way?
+=begin
+TODO_HELP: How do I redirect this to 'user_sessions/new' without losing validation errors? user_session/new and user/new now occupy the same view (combined login and signup). Am I even doing that the right way?
+EXPLANATION:
+You can either redirect to the action (and lose @user as it's not stored in the db yet) or just render the view template as below.
+I've had to pass a @user_session and modify how the partials are called to make both forms work.
+I doing know enough to declare this right or wrong! It seems to work but I've not tested it fully. It might be a bit messy having three controllers for the whole process but I fear changes would cause conflicts with modules/ RESTful guidelines or some such.
+=end
+        @user_session = UserSession.new
+        format.html { render :template => "user_sessions/new" } 
+        # format.html { render "user_sessions/new" } #also valid
         format.json { render :json => @user.errors, :status => :unprocessable_entity }
       end
     end
